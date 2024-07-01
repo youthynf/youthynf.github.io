24f0cd70af53d2e5b0b5a7768293ad66d3b722a1
yannengfa <940436475@qq.com>
2024-07-02 07:56:01 +0800

    加入百度统计

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -14,6 +14,50 @@ export default defineUserConfig({
 
   theme,
 
+  head: [
+    // meta
+    ["meta", { name: "robots", content: "all" }],
+    ["meta", { name: "author", content: "youthynf" }],
+    [
+      "meta",
+      {
+        "http-equiv": "Cache-Control",
+        content: "no-cache, no-store, must-revalidate",
+      },
+    ],
+    ["meta", { "http-equiv": "Pragma", content: "no-cache" }],
+    ["meta", { "http-equiv": "Expires", content: "0" }],
+    [
+      "meta",
+      {
+        name: "keywords",
+        content:
+            "Java基础, 多线程, JVM, 虚拟机, 数据库, MySQL, Spring, Redis, MyBatis, 系统设计, 分布式, RPC, 高可用, 高并发",
+      },
+    ],
+    [
+      "meta",
+      {
+        name: "description",
+        content:
+            "「Java学习 + 面试指南」一份涵盖大部分后端程序员所需要掌握的核心知识",
+      },
+    ],
+    ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
+    // 添加百度统计
+    [
+      "script",
+      {},
+      `var _hmt = _hmt || [];
+      (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?8dad1b404d530969c5698f03bccf78bc";
+        var s = document.getElementsByTagName("script")[0]; 
+        s.parentNode.insertBefore(hm, s);
+      })();`,
+    ],
+  ],
+
   alias: {
     "@theme-hope/modules/blog/components/BlogHero": path.resolve(
       __dirname,
